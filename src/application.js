d9418825d9165076096cf3cbbb4254de9c7a45e2
cringuxd <193437229+cringuxd@users.noreply.github.com>
2025-04-01 12:26:42 -0500

    Set up todo item and project classes, instantiate default project and project list

---
+++ src/application.js
@@ -0,0 +1,93 @@
+//Handles application logic, including management of todo-items, project(s), and default project instantiation
+class project_list {
+    #projects;
+    constructor() {
+        this.#projects = [];
+    }
+    get projects() {
+        return this.#projects;
+    }
+    addProject(proj) {
+        this.#projects.push(proj);
+    }
+    removeProject(proj) {
+        //modify code here
+    }
+}
+
+class project {
+    #todo_items;
+    constructor() {
+        this.#todo_items = [];
+    }
+    get todo_items() {
+        return this.#todo_items;
+    }
+    addTodo(todo) {
+        this.#todo_items.push(todo);
+    }
+    removeTodo(todo) {
+        //modify code here
+        this.#todo_items.pop(todo);
+    }
+}
+
+class todo_item {
+    #title;
+    #description;
+    #dueDate;
+    #priority;
+    #notes;
+    #isComplete = false;
+    constructor(title, description, dueDate, priority, notes) {
+        this.#title = title;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+        this.#notes = notes; 
+    }
+
+    get title() {
+        return this.#title;
+    }
+    set title(t) {
+        this.#title = t;
+    }
+
+    get description() {
+        return this.#description;
+    }
+    set description(desc) {
+        this.#description = desc;
+    }
+
+    get dueDate() {
+        return this.#dueDate;
+    }
+    set dueDate(date) {
+        this.#dueDate = date;
+    }
+
+    get priority() {
+        return this.#priority;
+    }
+    set priority(pri) {
+        this.#priority = pri;
+    }
+
+    get notes() {
+        return this.#notes;
+    }
+    set notes(n) {
+        this.#notes = n;
+    }
+
+    get isComplete() {
+        return this.#isComplete;
+    }
+    set isComplete(comp) {
+        this.#isComplete = comp;
+    }
+    
+}
+export {project_list, project, todo_item};
--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-import "./styles.css"
-import { loadHomePage } from "./load-home.js"
-import { loadMenuPage } from "./load-menu.js"
-import { loadContactsPage } from "./load-contacts.js"
+import {project_list, project, todo_item} from "./application.js"
+//Project Structure:
 
-const container = document.querySelector("#content");
-loadHomePage();
+/*
+    information:
+    - todo-item: object with title, description, dueDate, priority, notes, and checklist
+    - projects: groups/"arrays" of todo-items, start with 1 default project
+    - projects will be held in their own array
 
-const homeButton = document.querySelector(".home")
-const menuButton = document.querySelector(".menu")
-const contactButton = document.querySelector(".contacts")
+    application logic:
+    <->Functions (todo-item)<->
+    - newTodo():
+    - setCompleteTodo():
+    - setTodoPriority():
+    - setTodoTitle():
+    - setTodoDescription():
+    - setTodoDueDate():
+    - setTodoNotes():
+    - setTodoChecklist():
 
-homeButton.addEventListener("click", () => {
-    destroyPage();
-    loadHomePage();
-}
-);
+    <->Functions (project)<->
+    - newProject():
 
-menuButton.addEventListener("click", () => {
-    destroyPage();
-    loadMenuPage();
-}
-);
+    DOM manipulation:
 
-contactButton.addEventListener("click", () => {
-    destroyPage();
-    loadContactsPage();
-}
-)
+    User Interface:
 
-function destroyPage() {
-    while(container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-}
+    Information Storage (Web Storage API/localStorage)
 
-export {container};
+*/
+
+//Create project list, add default project
+let projects = new project_list();
+projects.addProject(new project());
+projects.projects[0].addTodo(new todo_item("Default Todo", "This is a default todo item.", "01/01/2026", "high", "bla bla bla"));
+console.log(projects.projects[0].todo_items[0].title);
